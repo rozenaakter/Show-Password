aa578a8baec0d63dfb90714f9b8c50865026b62c
Rozena Akter <153695774+rozenaakter@users.noreply.github.com>
2024-10-29 22:10:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// input
+const input = document.querySelector('input');
+const eye = document.querySelector('#eye-icon');
+
+eye.addEventListener('click', () => {
+    if(input.type === 'password'){
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
+
+    
+
+});
+// input.type === 'password' ? (input.type ='text'): (input.type = 'password');
